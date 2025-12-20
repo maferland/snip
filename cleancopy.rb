c269dd700cf46597a87feae81201b570abfac05c
Marc-Antoine <marc-antoine.ferland@carta.com>
2025-12-19 22:10:02 -0500

    fix: cask audit compliance

--- cleancopy.rb
+++ cleancopy.rb
@@ -2,8 +2,7 @@ cask "cleancopy" do
   version "1.1.0"
   sha256 "75a5dc4ea9d990395d86c8c421ceb07c137b3d8beaee90e6bbf83cccffe1fcdc"
 
-  url "https://github.com/maferland/clean-copy/releases/download/v#{version}/CleanCopy-v#{version}-macos.dmg",
-      verified: "github.com/maferland/clean-copy/"
+  url "https://github.com/maferland/clean-copy/releases/download/v#{version}/CleanCopy-v#{version}-macos.dmg"
   name "CleanCopy"
   desc "Automatically strip tracking parameters from copied URLs"
   homepage "https://github.com/maferland/clean-copy"
